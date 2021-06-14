1d7be90c556fa6e984f008d9b25267eede1cee7b
Igor Egorov <isegorov@edu.hse.ru>
2021-06-14 21:21:28 +0300

    Fixed codestyle.

--- module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
+++ module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
@@ -105,8 +105,7 @@ SharedPtr<T>::SharedPtr(Y* p, Deleter deleter) noexcept
 
 template <typename T>
 SharedPtr<T>::SharedPtr(const SharedPtr& other) noexcept
-    : valuePointer(other.valuePointer), 
-    controlPointer(other.controlPointer) {
+    : valuePointer(other.valuePointer), controlPointer(other.controlPointer) {
     controlPointer->AddRef();
 }
 
